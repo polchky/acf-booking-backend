850f19d41ccf5b7599195e49b8447ce2877073c9
Pascal Gremaud <polchky@gmail.com>
2020-05-15 01:25:57 +0200

    Finish availabilities check

--- routes/bookings.js
+++ routes/bookings.js
@@ -3,10 +3,7 @@ const { Booking, Config } = require('@models');
 const { auth, param } = require('@middlewares');
 const utils = require('@utils');
 
-const getAvailabilities = async (date, locationObject) => {
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+const getAvailabilities = async (date, locationObject, role) => {
 
     // Init availabilities object
     const targets = ['default'];
@@ -15,6 +12,13 @@ const getAvailabilities = async (date, locationObject) => {
 
     for (let target = 0; target < targets.length; target += 1) {
         const targetK = targets[target];
+        let targetCapacity;
+        if (locationObject.targets && targetK !== 'default') {
+            const tar = locationObject.targets.find((t) => t.distance === targetK);
+            targetCapacity = tar.number;
+        } else {
+            targetCapacity = locationObject.capacity;
+        }
         av[targetK] = {};
         const time = new Date();
         time.setHours(7, 0, 0, 0);
@@ -24,14 +28,82 @@ const getAvailabilities = async (date, locationObject) => {
             av[targetK][timeK] = {};
             for (let minutes = 60; minutes <= 120; minutes += 30) {
                 const durationK = utils.getDuration(minutes);
-                av[targetK][timeK][durationK] = locationObject.capacity;
+                av[targetK][timeK][durationK] = targetCapacity;
             }
         }
     }
 
+    // Get bookings for selected day
+    const min = new Date(date);
+    min.setHours(7, 0, 0, 0);
+    const max = new Date(date);
+    max.setHours(22, 0, 0, 0);
+
     const bookings = await Booking.find({
-        time: today,
+        location: locationObject.name,
+        time: { $gte: min, $lte: max },
     });
+
+    // Decrement availabilities
+    for (let i = 0; i < bookings.length; i += 1) {
+        const target = bookings[i].target || 'default';
+        const time = utils.getHM(bookings[i].time);
+        const duration = utils.getDuration(bookings[i].duration);
+
+        // Decrement present and future times
+        for (let j = 0; j < bookings[i].duration; j += 30) {
+            const realTime = new Date(bookings[i].time);
+            realTime.setMinutes(realTime.getMinutes() + j);
+            const realTimeString = utils.getHM(realTime);
+            if (av[target][realTimeString] !== undefined) {
+                for (let k = 60; k <= 120; k += 30) {
+                    av[target][realTimeString][utils.getDuration(k)] -= 1;
+                }
+            }
+        }
+
+        // Decrement previous times
+        for (let j = 30; j <= 90; j += 30) {
+            const realTime = new Date(bookings[i].time);
+            realTime.setMinutes(realTime.getMinutes() - j);
+            const realTimeString = utils.getHM(realTime);
+            for (let k = 120; k > j; k -= 30) {
+                if (av[target][realTimeString] !== undefined) {
+                    av[target][realTimeString][utils.getDuration(k)] -= 1;
+                }
+            }
+        }
+    }
+
+    // Remove full days and Jura empty days
+    for (let i = 0; i < targets.length; i += 1) {
+        const times = Object.keys(av[targets[i]]);
+        for (let j = 0; j < times.length; j += 1) {
+            const durations = Object.keys(av[targets[i]][times[j]]);
+            for (let k = 0; k < durations.length; k += 1) {
+                // Remove empty session for Jura
+                if (
+                    targets[i] === 'Jura'
+                    && role === 'user'
+                    && av[targets[i]][times[j]][durations[k]] === locationObject.capacity
+                ) {
+                    av[targets[i]][times[j]][durations[k]] = 0;
+                }
+                // Remove full durations
+                if (av[targets[i]][times[j]][durations[k]] <= 0) {
+                    delete av[targets[i]][times[j]][durations[k]];
+                }
+            }
+            // Remove empty times
+            if (Object.keys(av[targets[i]][times[j]]).length === 0) {
+                delete av[targets[i]][times[j]];
+            }
+        }
+        // Remove empty targets
+        if (Object.keys(av[targets[i]]).length === 0) {
+            delete av[targets[i]];
+        }
+    }
 
     return av;
 };
@@ -61,7 +133,7 @@ router
 
         if (ctx.request.query.to !== undefined) {
             to = new Date(ctx.request.query.to);
-            to.setHours(0, 0, 0, 0);
+            to.setHours(23, 0, 0, 0);
             ctx.assert(!Number.isNaN(to.getTime()), 400);
         } else {
             to = new Date(today);
@@ -70,7 +142,8 @@ router
 
         ctx.body = await Booking.find({
             time: { $gte: from, $lte: to },
-        }).populate('user');
+        }).populate('user', 'username');
+
     })
 
     .get('/available', async (ctx) => {
@@ -87,7 +160,7 @@ router
         const locationObject = config.locations.find((l) => l.name === location);
         ctx.assert(locationObject !== null, 400);
 
-        ctx.body = await getAvailabilities(date, locationObject);
+        ctx.body = await getAvailabilities(date, locationObject, ctx.state.user.role);
     })
 
     .post('/', async (ctx) => {
@@ -113,7 +186,7 @@ router
 
         const day = new Date(time);
         day.setHours(0, 0, 0, 0);
-        const availabilities = await getAvailabilities(day, locationObject);
+        const availabilities = await getAvailabilities(day, locationObject, ctx.state.user.role);
 
         try {
             const times = availabilities[body.target || 'default'];
